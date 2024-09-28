29ec6a39af5ec5956e295709054c0b04323168a1
Juan Carlos <juancarlojuan@gmail.com>
2024-09-28 22:00:27 +0200

    new ino

---
+++ Henrrique/Henrrique.ino
@@ -0,0 +1,71 @@
+
+
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(8,9,10,11,12,13);
+int P=6; //POWER SWITCH
+int T=A2; // A2 has already been defined as Analog Pin 2 in arduino lib
+int L=A4; // A4 has already been defined as Analog Pin 4 in arduino lib
+void setup()
+{
+   lcd.begin(16,2);
+   pinMode(P, INPUT);
+
+}
+
+////////////////// TEMPERATURE////////////////
+ 
+void Temperature()
+{
+  int value_temp=analogRead(T); //reading from A2 pin
+  float milivolts_temp=((value_temp/1023.0)*5000);
+  float CELSIUS=milivolts_temp/10;
+  lcd.setCursor(0,1); // column, row
+  lcd.print("T:");
+  lcd.print(CELSIUS);
+}
+
+/////////////// CAPTURE DE POwER STATUS AND DISPLAY
+
+void POWER()
+{
+    if(digitalRead(P)==LOW)
+    {
+      lcd.setCursor(0,0);// column, row
+      lcd.print("P:");
+      lcd.print("OFF");
+    }
+    else
+    {
+      lcd.setCursor(0,0);// column, row
+        lcd.print("P:");
+        lcd.print("ON");
+    }
+}
+
+///////////////Main funtion///////////////
+
+void loop()
+
+{
+    Temperature();
+    POWER();
+    Light();
+    delay(2000);
+}
+
+
+////////////////Light////////////////
+void Light()
+{
+  int Light_value=analogRead(L); //reading from A4 pin
+  float volts_lig=((Light_value/1023.0)*5);
+  int LIGHT=500/(4*((5-volts_lig)/volts_lig));
+  lcd.setCursor(6,0); // column, row
+  lcd.print("L:");
+  lcd.print(LIGHT);
+  lcd.print("lx");
+  delay(2000);
+  lcd.clear();
+}
+
+///////////////Humidity//////////////////
